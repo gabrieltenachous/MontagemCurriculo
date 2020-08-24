40b7dcfc613c2c71120c09e44ca17f2fd817d9d4
gabrieltenachous <gabriel_oliveira_f_sousa@outlook.com>
2020-08-24 10:51:36 -0300

    Criando as Classes e os requisitos

--- MontagemCurriculo/Models/Contexto.cs
+++ MontagemCurriculo/Models/Contexto.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MontagemCr.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,15 @@ namespace MontagemCurriculo.Models
 {
     public class Contexto:DbContext
     {
+        public DbSet<Curriculo> Curriculos { get; set; }
+        public DbSet<Idioma> Idiomas { get; set; }
+        public DbSet<TipoCurso> TiposCursos { get; set; }
+        public DbSet<FormacaoAcademica> FormacoesAcademicas { get; set; }
+        public DbSet<ExperienciaProfissional> ExperienciasProfissionais { get; set; }
+        public DbSet<Objetivo> Objetivos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<InformacaoLogin> InformacoesLogin { get; set; }
+
         public Contexto(DbContextOptions<Contexto> opcoes) : base(opcoes) { }
     }
 }
